7f2276da8ea2af3968d57774f4300747d5d0b5e4
Radu Bacila <Radu.Bacila@computacenter.com>
2023-08-25 15:59:46 +0300

    Updated environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,12 @@ export const environment: EnvironmentTypeModel = {
     rootUrl: '',
     msalConfig: {
         auth: {
-            // clientId: '05321182-2629-434e-b593-d8272c535df9',
             clientId: '70f4d3b4-24ee-4742-aea6-43808b2a391d',
-
             authority: 'https://login.microsoftonline.com/4f3de4dd-5a63-4f78-badf-657bbe2f7107'
         }
     },
     apiConfig: {
-        // scopes: ['api://5959ac77-d979-4a98-a27c-b6d473649578/Read'],
-        scopes: ['api://70f4d3b4-24ee-4742-aea6-43808b2a391d/Read'],
-
+        scopes: ['api://5959ac77-d979-4a98-a27c-b6d473649578/Read'],
         uri: 'https://technique.qa.spctrm.computacenter.com/api'
     }
 };
