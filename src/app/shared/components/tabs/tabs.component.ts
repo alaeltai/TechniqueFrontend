9bd209150289060fa2e0537e2a50e21b31a04ab8
Adrian Nistor <Adrian.Nistor@computacenter.com>
2023-09-04 16:04:15 +0300

    [Tabs]:  Show content and border tabs animations

--- src/app/shared/components/tabs/tabs.component.ts
+++ src/app/shared/components/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LineBreakPipe } from '@teq/shared/pipes/lineBreak.pipe';
 import { ReplaceCharPipe } from '@teq/shared/pipes/replaceChar.pipe';
@@ -14,18 +14,33 @@ export interface ITab {
     templateUrl: './tabs.component.html',
     styleUrls: ['./tabs.component.scss']
 })
-export class TabsComponent {
+export class TabsComponent implements OnChanges {
     public selectedIndex = 0;
+    public animateContent = true;
 
     @Input() tabs!: ITab[];
+
+    @Input() item: unknown;
+
+    ngOnChanges(changes: SimpleChanges): void {
+        changes['item']?.currentValue !== changes['item']?.previousValue && this.toggleContentAnimation();
+    }
 
     changeTab(index: number): void {
         if (index !== this.selectedIndex) {
             this.selectedIndex = index;
+            this.toggleContentAnimation();
         }
     }
 
     trackTabName(_i: number, tab: ITab): string {
         return tab.title;
+    }
+
+    toggleContentAnimation(): void {
+        this.animateContent = false;
+        setTimeout(() => {
+            this.animateContent = true;
+        });
     }
 }
