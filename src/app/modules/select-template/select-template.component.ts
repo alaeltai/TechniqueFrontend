b504fba236fa49f3767b3b4058b73a03fe6f16ae
Adrian Nistor <Adrian.Nistor@computacenter.com>
2023-09-18 10:05:16 +0300

    [TreeViewer & Column Viewer]: US 2172 - Color framing for methods [SelectTemplate]:  US 2170 - Create area - add a button "start with full framework"

--- src/app/modules/select-template/select-template.component.ts
+++ src/app/modules/select-template/select-template.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SelectTemplateService } from '@teq/modules/select-template/state/select-template.service';
 import { ITemplateCard } from '@teq/modules/select-template/types/template-card.type';
 import { bottomFadeIn, fadeIn } from '@teq/shared/animations/animations.lib';
+import { FiltersService } from '@teq/shared/components/filters/filters.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -13,11 +15,20 @@ import { Observable } from 'rxjs';
 export class SelectTemplateComponent implements OnInit {
     public templates$!: Observable<ITemplateCard[]>;
 
-    constructor(private readonly _selectTemplateService: SelectTemplateService) {}
+    constructor(
+        private readonly _selectTemplateService: SelectTemplateService,
+        private readonly _filtersService: FiltersService,
+        private readonly _router: Router
+    ) {}
 
     ngOnInit(): void {
         this._selectTemplateService.loadTemplateCards();
 
         this.templates$ = this._selectTemplateService.templates$;
+    }
+
+    startFullFramework(): void {
+        this._filtersService.reset();
+        void this._router.navigate(['/create']);
     }
 }
